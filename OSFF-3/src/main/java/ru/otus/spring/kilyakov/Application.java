7b7a082df41bac618097c3499e632c9ff3daed24
Dmitriy B. Kilyakov <d.kilyakov@kck.ru>
2022-10-23 11:48:27 +0300

    OSFH-4 Spring Shell + ansi colors

--- OSFF-3/src/main/java/ru/otus/spring/kilyakov/Application.java
+++ OSFF-3/src/main/java/ru/otus/spring/kilyakov/Application.java
@@ -3,14 +3,11 @@ package ru.otus.spring.kilyakov;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import ru.otus.spring.kilyakov.service.impl.TestStarterImpl;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class Application {
     public static void main(String... args) {
         var ctx = SpringApplication.run(Application.class, args);
-        TestStarterImpl testingService = ctx.getBean(TestStarterImpl.class);
-        testingService.run();
     }
 }
---
+++ OSFF-3/src/main/java/ru/otus/spring/kilyakov/shell/ShellService.java
@@ -0,0 +1,7 @@
+package ru.otus.spring.kilyakov.shell;
+
+public interface ShellService {
+    public String login(String login);
+
+    public String run();
+}
