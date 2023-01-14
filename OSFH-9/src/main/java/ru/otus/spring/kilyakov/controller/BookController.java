0dd3710657025b1b8bad5a5d1be2124e8912f317
Dmitriy B. Kilyakov <d.kilyakov@kck.ru>
2023-01-14 21:56:43 +0300

    OSFH-9 delete book

--- OSFH-9/src/main/java/ru/otus/spring/kilyakov/controller/BookController.java
+++ OSFH-9/src/main/java/ru/otus/spring/kilyakov/controller/BookController.java
@@ -6,9 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.spring.kilyakov.domain.Author;
-import ru.otus.spring.kilyakov.domain.Book;
 import ru.otus.spring.kilyakov.domain.Genre;
 import ru.otus.spring.kilyakov.dto.BookDto;
 import ru.otus.spring.kilyakov.service.AuthorService;
@@ -18,6 +18,7 @@ import ru.otus.spring.kilyakov.service.GenreService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
 
@@ -39,27 +40,6 @@ public class BookController {
         return "read";
     }
 
-    @GetMapping("/edit")
-    public String editBook(@RequestParam("id") long id, Model model) {
-        BookDto book = bookService.getById(id);
-        List<Author> authors = authorService.getAll();
-        List<Genre> genres = genreService.getAll();
-        model.addAttribute("authors", authors);
-        model.addAttribute("genres", genres);
-        model.addAttribute("book", book);
-        return "edit";
-    }
-
-    @PostMapping("/edit")
-    public String updateBook(BookDto book,
-                           BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            return "edit";
-        }
-        bookService.update(book.toDomainObject());
-        return "redirect:/";
-    }
-
     @GetMapping("/add")
     public String addBook(Model model) {
         BookDto bookDto = new BookDto();
@@ -73,11 +53,38 @@ public class BookController {
 
     @PostMapping("/add")
     public String createBook(BookDto book,
-                           BindingResult bindingResult, Model model) {
+                             BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "edit";
         }
         bookService.save(book.toDomainObject());
-        return "redirect:/";
+        return "redirect:/book/";
+    }
+
+    @GetMapping("/edit")
+    public String editBook(@RequestParam("id") long id, Model model) {
+        BookDto book = bookService.getById(id);
+        List<Author> authors = authorService.getAll();
+        List<Genre> genres = genreService.getAll();
+        model.addAttribute("authors", authors);
+        model.addAttribute("genres", genres);
+        model.addAttribute("book", book);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String updateBook(BookDto book,
+                             BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
+        bookService.update(book.toDomainObject());
+        return "redirect:/book/";
+    }
+
+    @PostMapping("/delete")
+    public String deleteBook(@RequestParam("id") long id, Model model) {
+        bookService.deleteById(id);
+        return "redirect:/book/";
     }
 }
