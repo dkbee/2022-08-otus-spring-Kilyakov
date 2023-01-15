a8e50a93e9ee653e668c98dd1ceba4c0c0271443
Dmitriy B. Kilyakov <d.kilyakov@kck.ru>
2023-01-15 21:53:13 +0300

    OSFH-9 templates refactoring

--- OSFH-9/src/main/resources/data.sql
+++ OSFH-9/src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into genres (name) values ('Tutorial');
 insert into genres (name) values ('Story');
 
 insert into books (name, author_id, genre_id) values ('ABC', 2, 1);
-insert into books (name, author_id, genre_id) values ('123', 2, 1);
+insert into books (name, author_id, genre_id) values ('123', 1, 2);
 
 insert into comments (comment, book_id) values ('blablabla', 1);
 insert into comments (comment, book_id) values ('tralala', 1);
