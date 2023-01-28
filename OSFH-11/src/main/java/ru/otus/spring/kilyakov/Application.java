5800ea4701067267f9af2612e0b1d0eec7d982f0
Dmitriy B. Kilyakov <d.kilyakov@kck.ru>
2023-01-28 10:46:51 +0300

    OSFH-11  refactoring, testing

--- OSFH-11/src/main/java/ru/otus/spring/kilyakov/Application.java
+++ OSFH-11/src/main/java/ru/otus/spring/kilyakov/Application.java
@@ -1,17 +1,56 @@
 package ru.otus.spring.kilyakov;
 
-import io.mongock.runner.springboot.EnableMongock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import ru.otus.spring.kilyakov.domain.Author;
+import ru.otus.spring.kilyakov.domain.Book;
+import ru.otus.spring.kilyakov.domain.Comment;
+import ru.otus.spring.kilyakov.domain.Genre;
+import ru.otus.spring.kilyakov.repository.AuthorRepository;
+import ru.otus.spring.kilyakov.repository.BookRepository;
+import ru.otus.spring.kilyakov.repository.GenreRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
-@EnableMongock
 @EnableMongoRepositories
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        var context = SpringApplication.run(Application.class, args);
+        var bookRepository = context.getBean(BookRepository.class);
+        var authorRepository = context.getBean(AuthorRepository.class);
+        var genreRepository = context.getBean(GenreRepository.class);
+
+        authorRepository.saveAll(Arrays.asList(
+                Author.builder().id("1").firstName("Barnabus").lastName("Stinson")
+                        .build(), Author.builder().id("2").firstName("Petr").middleName("Ivanovich").build()
+        )).subscribe(author -> logger.info("author :{}", author));
+
+        genreRepository.saveAll(Arrays.asList(
+                Genre.builder().id("1").name("Manual").build(), Genre.builder().id("2").name("Story").build()
+        )).subscribe(genre -> logger.info("genre :{}", genre));
+
+        List<Comment> comments = new ArrayList<>();
+        comments.add(Comment.builder().comment("blablabla").build());
+
+        bookRepository.save(Book.builder()
+                .id("1")
+                .name("Bro Code")
+                .author(Author.builder().id("1").firstName("Barnabus").lastName("Stinson")
+                        .build())
+                .genre(Genre.builder().id("1").name("Manual").build())
+                .comments(comments)
+                .build()).subscribe(book -> logger.info("book :{}", book));
+
         System.out.printf("%n%s%n", "http://localhost:8080/all");
     }
 }
---
+++ OSFH-11/src/test/java/ru/otus/spring/kilyakov/BookReactiveControllerTest.java
@@ -0,0 +1,166 @@
+package ru.otus.spring.kilyakov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.otus.spring.kilyakov.controller.reactive.BookReactiveController;
+import ru.otus.spring.kilyakov.domain.Author;
+import ru.otus.spring.kilyakov.domain.Book;
+import ru.otus.spring.kilyakov.domain.Genre;
+import ru.otus.spring.kilyakov.dto.BookDto;
+import ru.otus.spring.kilyakov.repository.BookRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@WebFluxTest(BookReactiveController.class)
+class BookReactiveControllerTest {
+
+    @MockBean
+    BookRepository bookRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        Author author = Author.builder()
+                .id("1")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .build();
+        Genre genre = Genre.builder()
+                .id("1")
+                .name("Story")
+                .build();
+        Book bookDto = Book.builder()
+                .id("1")
+                .name("Test")
+                .author(author)
+                .genre(genre)
+                .build();
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(bookDto);
+        when(bookRepository.findById(anyString())).thenReturn(Mono.just(bookDto));
+        when(bookRepository.save(any())).thenReturn(Mono.just(bookDto));
+        when(bookRepository.findAll()).thenReturn(
+                Flux.fromIterable(bookList));
+        when(bookRepository.deleteById(anyString())).thenReturn(ServerResponse.ok().build().then());
+    }
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void insertBookTest() {
+        Author author = Author.builder()
+                .id("1")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .build();
+        Genre genre = Genre.builder()
+                .id("1")
+                .name("Story")
+                .build();
+        BookDto bookDto = BookDto.builder()
+                .id("1")
+                .name("Test")
+                .author(author)
+                .genre(genre)
+                .build();
+
+        webClient.post()
+                .uri("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(bookDto))
+                .exchange()
+                .expectStatus()
+                .isOk();
+        verify(bookRepository, times(1)).save(bookDto.toDomainObject());
+    }
+
+    @Test
+    public void getBookTest() {
+        Author author = Author.builder()
+                .id("1")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .build();
+        Genre genre = Genre.builder()
+                .id("1")
+                .name("Story")
+                .build();
+        BookDto excepted = BookDto.builder()
+                .id("1")
+                .name("Test")
+                .author(author)
+                .genre(genre)
+                .build();
+
+        var result = webClient.get()
+                .uri("/book/1")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(BookDto.class);
+
+        BookDto actual = result.getResponseBody().blockFirst();
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(excepted.getName(), actual.getName());
+    }
+
+    @Test
+    public void getAllBooksTest() {
+
+        var result = webClient.get()
+                .uri("/book")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(BookDto.class);
+        List<BookDto> actual = result.getResponseBody().collectList().block();
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals(1, actual.size());
+    }
+
+    @Test
+    public void updateBookTest() {
+        Author author = Author.builder()
+                .id("1")
+                .firstName("Ivan")
+                .lastName("Ivanov")
+                .build();
+        Genre genre = Genre.builder()
+                .id("1")
+                .name("Story")
+                .build();
+        BookDto bookDto = BookDto.builder()
+                .id("1")
+                .name("Test")
+                .author(author)
+                .genre(genre)
+                .build();
+        webClient.put()
+                .uri("/book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(bookDto))
+                .exchange()
+                .expectStatus()
+                .isOk();
+        verify(bookRepository, times(1)).save(bookDto.toDomainObject());
+    }
+
+    @Test
+    public void deleteBookTest() {
+        webClient.delete().uri("/book/1").exchange().expectStatus().isOk();
+        verify(bookRepository, times(1)).deleteById("1");
+    }
+}
